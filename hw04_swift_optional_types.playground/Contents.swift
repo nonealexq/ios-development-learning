7327a838ab5a7810acc10ccdea713c2718533d6c
Чичук Алексей <chichuk@tochka.com>
2021-10-05 19:35:19 +0500

    after code-review

--- hw04_swift_optional_types.playground/Contents.swift
+++ hw04_swift_optional_types.playground/Contents.swift
@@ -20,45 +20,59 @@
 /*
  п.1 :
  */
-let valuesTuple: (firstValue: String, secondValue: Int?, thirdValue: String, fourthValue: Double?, fifthValue: String) = ("1233", nil, "just a string", 9123.231, "96167fc9-2647-42e3-ac00-54e1f8980da2")
+/*
+ Тюпл максимум в 3 переменных, в идеале в 2, все что больше - дурной тон) Яблоки дают создать до 7, но это уже извращение)
+ Плюс в задании 5 констант, у тебя по факту 1 - valuesTuple, плюс константы должны быть String, у тебя есть и double и nil,
+ плюс нужен опциональный биндинг(фига се назвали) - опциональная цепочка, ее нет)
+ ну и задание получить всю сумму, тут лучше вынести в функцию с сигнатурой
+ func test(constants: String...) -> Int {
+    где constants именно такого типа, забыл как называется
+    P.S. если до изучения этого не дошел, можно без функции
+ }
+*/
 
-var sumStrings: Int     = 0
-let elseMessage: String = "is not a number"
+let firstValue: String      = "1233"
+let secondValue: String?    = nil
+let thirdValue: String      = "just a string"
+let fourthValue: String?    = "9123"
+let fifthValue: String      = "96167fc9-2647-42e3-ac00-54e1f8980da2"
+
+var sumStrings: Int         = 0
+let elseMessage: String     = "is not a number"
 
 // If firstValue ?? Int --> add to sumStrings
-if Int(valuesTuple.firstValue) != nil {
-    sumStrings = sumStrings + Int(valuesTuple.firstValue)!
-    print(sumStrings)
+if let intValue = Int(firstValue) {
+    sumStrings = sumStrings + intValue
 } else {
-    print(valuesTuple.firstValue, elseMessage)
+    print(firstValue, elseMessage)
 }
 
 // If secondValue ?? Int --> add to sumStrings
-if let starSecond = valuesTuple.secondValue {
-    sumStrings = sumStrings + starSecond
+if let intValue = secondValue {
+    sumStrings = sumStrings + Int(intValue)!
 } else {
-    print(valuesTuple.secondValue ?? "empty", elseMessage)
+    print(secondValue ?? "empty", elseMessage)
 }
 
 // If thirdValue ?? Int --> add to sumStrings
-if Int(valuesTuple.thirdValue) != nil {
-    sumStrings = sumStrings + Int(valuesTuple.thirdValue)!
+if let intValue = Int(thirdValue) {
+    sumStrings = sumStrings + intValue
 } else {
-    print(valuesTuple.thirdValue, elseMessage)
+    print(thirdValue, elseMessage)
 }
 
 // If fourthValue ?? Int --> add to sumStrings
-if let starFouth = valuesTuple.fourthValue{
-    sumStrings = sumStrings + Int(starFouth)
+if let intValue = Int(fourthValue!) {
+    sumStrings = sumStrings + intValue
 } else {
-    print(valuesTuple.fourthValue ?? "empty", elseMessage)
+    print(fourthValue ?? "empty", elseMessage)
 }
 
 // If fifthValue ?? Int --> add to sumStrings
-if Int(valuesTuple.fifthValue) != nil {
-    sumStrings = sumStrings + Int(valuesTuple.fifthValue)!
+if let intValue = Int(fifthValue) {
+    sumStrings = sumStrings + intValue
 } else {
-    print(valuesTuple.fifthValue, elseMessage)
+    print(fifthValue, elseMessage)
 }
 
 print("Summary: \(sumStrings)")
@@ -71,8 +85,12 @@ let defaultSuccessMessage: String   = "Default success message"
 
 var response: (statusCode: Int, message: String?, errorMessage: String?)
 
-response.statusCode = 404; response.message = nil; response.errorMessage = "Page not found :("
+// Дурной тон ставить ;, лучше с новой строки
+response.statusCode = 404;
+response.message = nil;
+response.errorMessage = "Page not found :("
 
+// По условию проверка на 200, можно конечно и так, но лучше явно как в задаче)
 if  (response.statusCode < 200) || (response.statusCode > 399) {
     print("Oops, status code: \(response.statusCode), failed with error message:  \(response.errorMessage ?? defaultErrorMessage)")
 } else {
@@ -82,11 +100,23 @@ if  (response.statusCode < 200) || (response.statusCode > 399) {
 /*
  п.3 :
  */
-var withoutCarText: String      = "No car"
-var withoutExamMarkText: String = "Didn't show up for the exam"
 
-var firstStudentInfo: (name: String?, carRegNumber: String?, grade: String?) = (nil, nil, nil)
-var secondStudentInfo: (name: String?, carRegNumber: String?, grade: String?) = (nil, nil, nil)
+// Свойства не меняются, поэтому лучше let
+let withoutCarText: String      = "No car"
+let withoutExamMarkText: String = "Didn't show up for the exam"
+
+/*
+ Чтобы два раза не дублировать
+ typealias Student = (name: String?, carRegNumber: String?, grade: String?)
+ var firstStudentInfo: Student = (nil, nil, nil)
+ 
+ P.S. Если изучал enum, то для grade он лучше подойдет, в стрингу можно запихнуть все что угодно, а вот enum
+ с ограниченными регистрами не даст этого сделать
+*/
+
+typealias Student = (name: String?, carRegNumber: String?, grade: String?)
+var firstStudentInfo: Student = (nil, nil, nil)
+var secondStudentInfo: Student = (nil, nil, nil)
 
 /*
  И тут начался экзамен,
@@ -100,6 +130,8 @@ firstStudentInfo.grade          = "5"
 secondStudentInfo.name          = "Ivan"
 secondStudentInfo.carRegNumber  = "АА891А196"
 
+// Можно круче https://www.hackingwithswift.com/articles/178/super-powered-string-interpolation-in-swift-5-0,
+// но на практике не приходилось использовать, поэтому можно не юзать, а можно попробовать и знать что так можно сделать)
 print("\nInfo about first student:\n" +
       "\tname: \(firstStudentInfo.name ?? "Name is empty")\n" +
       "\tcar reg number: \(firstStudentInfo.carRegNumber ?? withoutCarText)\n" +
